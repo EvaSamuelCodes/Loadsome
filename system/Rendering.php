8153b0f375e52ecbda7f1fa8bfd86ac028b9101d
EvaLynn Samuel <HowAwesomeIsEvey@Gmail.com>
2019-11-03 01:32:29 -0500

    More debug code... gone.

--- system/Rendering.php
+++ system/Rendering.php
@@ -7,7 +7,7 @@ trait Rendering
             exit("<h1>You can't render a view unless your method has called get_route() </h1><p>Sorry</p>");
         } else {
             $view = MODULE_ROOT . '/' . $this->route['module'] . '/app/view/' . $this->route['class'] . '/' . $this->route['method'] . '.php';
-            $this->pretty($view);
+            //$this->pretty($view);
             if (file_exists($view)) {
                 require_once $view;
             } else {
