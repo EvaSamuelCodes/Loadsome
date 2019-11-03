08be84a39e72f934ffc3a1f76ca89f1fbad499ee
EvaLynn Samuel <HowAwesomeIsEvey@Gmail.com>
2019-11-03 01:20:23 -0500

    typo

--- system/System.php
+++ system/System.php
@@ -2,11 +2,12 @@
 
 // Resoning behind creating a core class
 // 1. It's light, makes it so that I don't have to declare and redeclare dependencies.
-// 2. It's in the middle, so if there's a piece of functionality that needs to go everywhere... 
+// 2. It's in the middle, so if there's a piece of functionality that needs to go everywhere...
 //    this is an easy way to go about doing it.
 
-class System{
+class System
+{
     use Helpers;
     use Routing;
- //   use Rendering;
+    use Rendering;
 }
