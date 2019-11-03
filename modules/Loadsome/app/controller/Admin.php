ab02aad3253ebe8b9f86fe085e465f535ef079ea
EvaLynn Samuel <HowAwesomeIsEvey@Gmail.com>
2019-11-03 00:25:05 -0500

    Fixing link, adding variables.

--- modules/Loadsome/app/controller/Admin.php
+++ modules/Loadsome/app/controller/Admin.php
@@ -3,10 +3,12 @@
     class Admin extends Core{
         public function index(){
             $route = $this->get_route();
-            print "Hi world. <a href='/modules/Loadsome/Admin/web_page'>Link</a>";
+            print "Hi world. <a href='/Loadsome/Admin/web_page/1/2/3/4/5/6'>Link</a>";
             $this->pretty($route);
         }
         public function web_page(){
+            $route = $this->get_route();
             print "I am another method.";
+            $this->pretty($route);
         }
     }
