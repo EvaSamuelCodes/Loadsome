644c5146cb35314f2c5463c66b79dc17ca0f6881
EvaLynn Samuel <HowAwesomeIsEvey@Gmail.com>
2019-11-03 00:59:10 -0500

    Making views prettier.

--- modules/Loadsome/app/controller/Admin.php
+++ modules/Loadsome/app/controller/Admin.php
@@ -3,12 +3,8 @@
     class Admin extends Core{
         public function index(){
             $route = $this->get_route();
-            print "Hi world. <a href='/Loadsome/Admin/web_page/1/2/3/4/5/6'>Link</a>";
-            $this->pretty($route);
         }
         public function web_page(){
             $route = $this->get_route();
-            print "I am another method.";
-            $this->pretty($route);
         }
     }
--- modules/Loadsome/app/view/Admin/index.php
+++ modules/Loadsome/app/view/Admin/index.php
@@ -1,2 +1,9 @@
 <h1>HI, I'm the Admin Index View</h1>
-<p>Doh dee, do. Forgot to exclude me, didn'cha?</p>
+<p>
+    Doh dee, do. Forgot to exclude me, didn'cha? <br/>
+    <a href="/Loadsome/Admin/web_page/1/2/3/4/5/6">Linkees</a>
+</p>
+
+<?php
+$this->pretty($route);
+?>
