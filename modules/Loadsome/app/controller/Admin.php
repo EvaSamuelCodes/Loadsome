d5765e5855f54b3005ef2a8c86b738b18fbc8d62
EvaLynn Samuel <HowAwesomeIsEvey@Gmail.com>
2019-11-03 00:06:10 -0500

    Let's try a non default method.

--- modules/Loadsome/app/controller/Admin.php
+++ modules/Loadsome/app/controller/Admin.php
@@ -3,7 +3,10 @@
     class Admin extends Core{
         public function index(){
             $route = $this->get_route();
-            print "Hi world.";
+            print "Hi world. <a href='/modules/Loadsome/Admin/web_page'>Link</a>";
             $this->pretty($route);
+        }
+        public function web_page(){
+            print "I am another method.";
         }
     }
